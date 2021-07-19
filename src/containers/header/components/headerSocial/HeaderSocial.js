b8d7940f299e4ff28a714544b8d210a4328cdab9
Andrey Leonets <awdenheimlee@gmail.com>
2021-07-19 18:36:20 +0300

    Feat (modalWindow): install material-ui and implement modal window

--- src/containers/header/components/headerSocial/HeaderSocial.js
+++ src/containers/header/components/headerSocial/HeaderSocial.js
@@ -1,12 +1,11 @@
 import React from 'react';
+import InstagramIcon from '@material-ui/icons/Instagram';
 
 export const HeaderSocial = () => {
   return(
     <div className="header-social">
       <a className="instagram" href="https://www.instagram.com/new_kids_english/" title="New Kids English in Instagram" target="_blank" rel="noreferrer">
-        <span className="instagram-icon">
-          <i className="fab fa-instagram"></i>
-        </span>
+        <InstagramIcon fontSize="large" />
         <p className="instagram-title">Our Instagram</p>
       </a>
     </div>
--- src/containers/header/components/modalWindow/ModalWindow.js
+++ src/containers/header/components/modalWindow/ModalWindow.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 import './modalWindow.scss';
 
@@ -12,31 +14,22 @@ export const ModalWindow = () => {
     <>
       { !modalIsOpen ?
         <button onClick={() => setModalIsOpen(true)} className="modal-button">
-          Open
+          <MenuIcon />
         </button> :
         <button onClick={() => setModalIsOpen(false)} className="modal-button">
-          Close
+          <CloseIcon />
         </button>
       }
       <Modal 
         isOpen={modalIsOpen}
         style={{
           overlay: {
-            // position: 'fixed',
-            // top: 0,
-            // left: 0,
-            // right: 0,
-            // bottom: 0,
-            // backgroundColor: 'grey',
-            // backgroundColor: '#5f5f5f',
-            bottom: 0,
-            height: '100vh',
-            left: 0,
             position: 'fixed',
+            bottom: 0,
+            left: 0,
             right: 0,
             top: 0,
-            // transform: 'translateY(-100%)',
-            // transition: 'transform 0.2s',
+            height: '100vh',
             width: '100%'
           },
           content: {
@@ -47,20 +40,17 @@ export const ModalWindow = () => {
             alignItems: 'center',
             display: 'flex',
             flexDirection: 'column',
-            backgroundColor: '#5f5f5f',
-            // margin: '100px auto'
+            backgroundColor: '#5f5f5f'
           }
         }}  
       >
-        {/* <div id="modal" className="modal"> */}
-          <div className="modal-content">
-            <a href="/#" className="modal-menu-link" title="Home">Home</a>
-            <a href="/#features" className="modal-menu-link" title="What do we offer?">Our Features</a>
-            <a href="/#pricing" className="modal-menu-link" title="Price">Pricing</a>
-            <a href="/#about-us" className="modal-menu-link" title="About Us">About Us</a>
-            <a href="/#carrers" className="modal-menu-link" title="Carrers at New Kids English">Carrers</a>
-          </div>
-        {/* </div> */}
+        <div className="modal-content">
+          <a href="/#" className="modal-menu-link" title="Home" onClick={() => setModalIsOpen(false)}>Home</a>
+          <a href="/#features" className="modal-menu-link" title="What do we offer?" onClick={() => setModalIsOpen(false)}>Our Features</a>
+          <a href="/#pricing" className="modal-menu-link" title="Price" onClick={() => setModalIsOpen(false)}>Pricing</a>
+          <a href="/#about-us" className="modal-menu-link" title="About Us" onClick={() => setModalIsOpen(false)}>About Us</a>
+          <a href="/#carrers" className="modal-menu-link" title="Carrers at New Kids English" onClick={() => setModalIsOpen(false)}>Carrers</a>
+        </div>
       </Modal>
     </>
   );
